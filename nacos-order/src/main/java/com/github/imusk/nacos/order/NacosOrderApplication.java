6cf00ca013221eaaec92ab0b065d5fa2367374db
musk <muskcool@protonmail.com>
2021-07-29 20:57:25 +0800

    add: Nacos服务注册与发现(订单模块)

---
+++ nacos-order/src/main/java/com/github/imusk/nacos/order/NacosOrderApplication.java
@@ -0,0 +1,23 @@
+package com.github.imusk.nacos.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author: Musk
+ * @email: muskcool@protonmail.com
+ * @datetime: 2021-07-29 20:25
+ * @classname: NacosOrderApplication
+ * @description: 服务启动
+ */
+// 注册服务,使其他模块能够发现你
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosOrderApplication.class);
+    }
+
+}
